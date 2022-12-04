5cb3379f90819e31e72039e0405ff1a3e1e746bb
bmuller18 <bmuller18@gmail.com>
2022-12-04 11:09:06 -0500

    Social Networks button configure

--- MainMenu.py
+++ MainMenu.py
@@ -1,41 +1,64 @@
 from tkinter import * 
 from PIL import ImageTk, Image
+import time
+
+
+
 root = Tk()
+root.title("")
+# Link social buttons to the network
+def my_command():
+    print("fr")
+
 #IMAGES
-fbimage=Image.open('C:/Users/brand/Desktop/Practicas/Ongit/menuapp/Fb.png')
-fb=fbimage.resize((50,50))
-fb=ImageTk.PhotoImage(fb)
-instaimg=Image.open('C:/Users/brand/Desktop/Practicas/Ongit/menuapp/Insta.png')
-ins=instaimg.resize((50,50))
-ins=ImageTk.PhotoImage(ins)
-snapimage=Image.open('C:/Users/brand/Desktop/Practicas/Ongit/menuapp/snapchat.png')
-sn=snapimage.resize((50,50))
-sn=ImageTk.PhotoImage(sn)
-twimage=Image.open('C:/Users/brand/Desktop/Practicas/Ongit/menuapp/twitter.png')
-tw=twimage.resize((50,50))
-tw=ImageTk.PhotoImage(tw)
+fbimage=Image.open('Resources/Fb.png')
+fb=fbimage.resize((80,80))
+fb=ImageTk.PhotoImage(fb)#FACEBOOK
+instaimg=Image.open('Resources/Insta.png')
+ins=instaimg.resize((80,80))
+ins=ImageTk.PhotoImage(ins)#INSTAGRAM
+snapimage=Image.open('Resources/snapchat.png')
+sn=snapimage.resize((80,80))
+sn=ImageTk.PhotoImage(sn)#SNAPCHCHAT
+twimage=Image.open('Resources/twitter.png')
+tw=twimage.resize((80,80))
+tw=ImageTk.PhotoImage(tw)#TWITTER
 
-
-botimg = Image.open("C:/Users/brand/Desktop/Practicas/Ongit/menuapp/bottom.jpg")
-bot = botimg.resize((800,500))
+botimg = Image.open("Resources/bottom.jpg")
+bot = botimg.resize((800,300))
 bot = ImageTk.PhotoImage(bot)
 
 
 
-root.geometry("800x800")
+root.geometry("800x500")
 bottomlbl = Frame(root, bg='white', height=800, width=800)
-uplbl = Label(bottomlbl, bg= '#FCFCFC')
+uplbl = Frame(bottomlbl, bg= '#FFF')
+timer = Label(uplbl)
 
-lb = Label(uplbl, height=250, width=200, image=fb, bg='#D1D1D1').grid(row=1, column=0)
-lb2 = Label(uplbl, height=250, width=200, image=ins, bg='#D1D1D1').grid(row=1, column=1)
-lb3 = Label(uplbl, height=250, width=200, image=sn, bg='#D1D1D1').grid(row=1, column=2)
-lb4 = Label(uplbl, height=250, width=200, image=tw, bg='#D1D1D1').grid(row=1, column=3)
+#Clock
+def clock():
+    hour = time.strftime("%H")
+    minute = time.strftime("%M")
+    seconds = time.strftime("%S")
+
+
+    timer.config(text=hour + ":" + minute  + ":" + seconds, bg='#FCFCFC', font=('verdana', 44, 'bold'))
+    timer.after(1000, clock)
+clock()
+
+#Let us create a dummy button and pass the image
+fbbutton= Button(uplbl, image=fb,command= my_command, height=250, width=200, borderwidth=0, bg='#FFF')
+fbbutton.grid(row=1, column=0)
+insbutton = Button(uplbl, image=ins, command=my_command, height=250, width=200, borderwidth=0, bg='#FFF')
+insbutton.grid(row=1, column=1)
+snbutton = Button(uplbl, image=sn, command= my_command, height=250, width=200, borderwidth=0, bg='#FFF')
+snbutton.grid(row=1, column=2)
+twbutton = Button(uplbl, image=tw, command= my_command, height=250, width=200, borderwidth=0, bg='#FFF')
+twbutton.grid(row=1, column=3)
+timer.grid(row=0, column=0, columnspan=5)
 uplbl.pack(fill = BOTH,)
 
-botlbl = Label(bottomlbl, image=bot, anchor=CENTER).pack(fill=X)
-
-
-
-
+#   Side bottom image
+botlbl = Label(bottomlbl, image=bot, anchor=CENTER).pack(side=BOTTOM)
 bottomlbl.pack()
 root.mainloop()
